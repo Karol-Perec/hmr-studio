1a8dea137d1e707cbadbfa62bdd6425a117ec71d
karol <karol.perec@gmail.com>
2020-02-14 16:31:45 +0100

    d

--- do_train.sh
+++ do_train.sh
@@ -7,9 +7,9 @@ DATA_DIR=datasets/tf_datasets/
 #CMD="python3 src/main.py --d_lr 1e-4 --e_lr 1e-5 --log_img_step 100 --pretrained_model_path=${PRETRAINED} --data_dir ${DATA_DIR} --e_loss_weight 60. --batch_size=64 --use_3d_label True --e_3d_weight 60. --datasets lsp,lsp_ext,mpii,coco,mpi_inf_3dhp --epoch 75 --log_dir logs"
 
 # To pick up training/training from a previous model, set LP
- LP='models/pre'
+LP='models/pre'
 # CMD="python -m src.main --d_lr 1e-4 --e_lr 1e-5 --log_img_step 1000 --load_path=${LP} --e_loss_weight 60. --batch_size=64 --use_3d_label True --e_3d_weight 60. --datasets lsp lsp_ext mpii h36m coco mpi_inf_3dhp --epoch 75"
-CMD="python3 src/main.py --d_lr 1e-4 --e_lr 1e-5 --log_img_step 100 --load_path=${LP} --pretrained_model_path=${PRETRAINED} --data_dir ${DATA_DIR} --e_loss_weight 60. --batch_size=64 --use_3d_label True --e_3d_weight 60. --datasets lsp,lsp_ext,mpii,coco,mpi_inf_3dhp,imdb --epoch 75 --log_dir logs"
+CMD="python3 src/main.py --d_lr 1e-4 --e_lr 1e-5 --log_img_step 100 --pretrained_model_path=${PRETRAINED} --data_dir ${DATA_DIR} --e_loss_weight 60. --batch_size=64 --use_3d_label True --e_3d_weight 60. --datasets lsp,lsp_ext,mpii,coco,mpi_inf_3dhp,imdb --epoch 75 --log_dir logs"
 
 
 echo $CMD
